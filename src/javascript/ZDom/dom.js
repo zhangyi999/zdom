7a592cdc7e5468fd1c6a29c37a8d1898cb9bf48e
zhangyi999 <zhang_btc@outlook.com>
2019-10-23 18:17:21 +0800

    add article

--- src/javascript/ZDom/dom.js
+++ src/javascript/ZDom/dom.js
@@ -18,7 +18,7 @@ function createDomTreeByPrototype(el) {
 }
 
 //继承模式，可以搞
-const el = 'button,nav,ruby,rt,rp,optgroup,svg,path,embed,object,frame,iframe,textarea,tbody,thead,hr,select,option,table,tr,th,dl,td,dd,div,li,ul,p,a,b,span,h1,h2,h3,h4,h5,h6,h7,s,i,input,img,form,canvas,section,main,footer,header,link,label,br'.split(',')
+const el = 'article,button,nav,ruby,rt,rp,optgroup,svg,path,embed,object,frame,iframe,textarea,tbody,thead,hr,select,option,table,tr,th,dl,td,dd,div,li,ul,p,a,b,span,h1,h2,h3,h4,h5,h6,h7,s,i,input,img,form,canvas,section,main,footer,header,link,label,br'.split(',')
 const DomPrototype = createDomTreeByPrototype(el)
 
 export default DomPrototype;
